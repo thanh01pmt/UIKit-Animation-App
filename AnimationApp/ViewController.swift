6a0f8f7a0ebd3f2789fed0cdba50f38941783e22
Tony Pham <thanh01.pmt@gmail.com>
2024-08-25 14:00:56 +0700

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Tony Pham on 25/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
