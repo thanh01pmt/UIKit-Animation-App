98595d965aa9454f14b7dc0c4f526d2504504ab8
Tony Pham <thanh01.pmt@gmail.com>
2024-08-25 23:05:39 +0700

    HP3-B2

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     
     var imgIndex: Int = 1
+    var timer: Timer!
+    var isAnimating = false
     
     @IBOutlet weak var imgGoodnight: UIImageView!
+    @IBOutlet weak var btnToggleAnimation: UIButton!
+    @IBOutlet weak var btnResetAnimation: UIButton!
     
     func updateImage() {
         print("goodnight-images/goodnight\(imgIndex)")
@@ -27,6 +31,45 @@ class ViewController: UIViewController {
         updateImage()
     }
     
+    @IBAction func toggleAnimating(_ sender: UIButton) {
+        if (isAnimating) {
+            isAnimating = false
+            btnToggleAnimation.tintColor = .systemBlue
+            btnToggleAnimation.setTitle("Animate", for: .normal)
+            pauseAnimation()
+            btnResetAnimation.isHidden = true
+        } else {
+            isAnimating = true
+            btnToggleAnimation.tintColor = .systemRed
+            btnToggleAnimation.setTitle("Pause", for: .normal)
+            startAnimation()
+            btnResetAnimation.isHidden = false
+        }
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.changeToNextImage(_:)), userInfo: nil, repeats: true)
+    }
+    
+    func stopTimer() {
+        timer.invalidate()
+    }
+    
+    func startAnimation() {
+        startTimer()
+    }
+    
+    func pauseAnimation() {
+        stopTimer()
+    }
+    
+    @IBAction func resetAnimation() {
+        pauseAnimation()
+        imgIndex = 1
+        updateImage()
+        toggleAnimating(btnToggleAnimation)
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -34,7 +77,6 @@ class ViewController: UIViewController {
         imgIndex = 1
         updateImage()
     }
-
 
 }
 
