fc0173944b579ecc88d3ba51965d7c8a02014484
Tony Pham <thanh01.pmt@gmail.com>
2024-08-25 22:12:19 +0700

    HP3-B1

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imgIndex: Int = 1
+    
+    @IBOutlet weak var imgGoodnight: UIImageView!
+    
+    func updateImage() {
+        print("goodnight-images/goodnight\(imgIndex)")
+        imgGoodnight.image = UIImage(named: "goodnight-images/goodnight\(imgIndex)")
+    }
+    
+    @IBAction func changeToNextImage(_ sender: UIButton) {
+        if (imgIndex >= 37) {
+            imgIndex = 1
+        } else {
+            imgIndex += 1
+        }
+        updateImage()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgIndex = 1
+        updateImage()
     }
 
 
